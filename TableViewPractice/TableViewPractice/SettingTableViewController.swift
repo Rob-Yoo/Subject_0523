4f7d2d6f7fd9249d5fca4b766bc42c6398c718fe
Rob-Yoo <jin12243@naver.com>
2024-06-03 20:17:16 +0900

    CASE 2에 Enum 적용

--- TableViewPractice/TableViewPractice/SettingTableViewController.swift
+++ TableViewPractice/TableViewPractice/SettingTableViewController.swift
@@ -7,37 +7,57 @@
 
 import UIKit
 
-class SettingTableViewController: UITableViewController {
+enum SettingOptions: Int, CaseIterable {
+    case total, personal, others
     
-    private var sectionTitles = ["전체 설정", "개인 설정", "기타"]
-    private var rowTitles = [
-        ["공지사항", "실험실", "버전 정보"],
-        ["개인/보안", "알림", "채팅", "멀티프로필"],
-        ["고객센터/도움말"]
-    ]
+    var mainOptions: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .others:
+            return "기타"
+        }
+    }
+    
+    var subOptions: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .others:
+            return ["고객센터/도움말"]
+        }
+    }
+}
+
+class SettingTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.rowHeight = 200
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return sectionTitles.count
+        return SettingOptions.allCases.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rowTitles[section].count
+        return SettingOptions.allCases[section].subOptions.count
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitles[section]
+        return SettingOptions.allCases[section].mainOptions
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "basic")!
         
-        cell.textLabel?.text = rowTitles[indexPath.section][indexPath.row]
+        cell.textLabel?.text = SettingOptions.allCases[indexPath.section].subOptions[indexPath.row]
         
         return cell
     }
